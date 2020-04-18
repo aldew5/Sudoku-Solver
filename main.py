f0fedb711df4309051d6d6f867b90ced9f1092f6
Alec Dewulf <alec@cisudbury.ca>
2020-04-18 19:18:07 -0400

    get_poss() now includes a check for other nums in the square

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
+"""
+BOX INDEXING ISN'T FUCKING GOD DAMN WORKING!!!
+"""
+
+import math
+
 def get_poss(puzzle, ri, ci):
     """returns a list possible numbers for a given cell"""
-    # check row
     poss = [1,2,3,4,5,6,7,8,9]
+
+    # check row
     for x in range(9):
-        if puzzle[ri][x] != 0 and puzzle[ri][x] in poss:
+        if puzzle[ri][x] in poss:
             poss.pop(poss.index(puzzle[ri][x]))
             
-
+    # check column      
     for row in puzzle:
         if row[ci] in poss:
             poss.pop(poss.index(row[ci]))
 
+    # set box index where a box is a 3x3 subsection
+    # of the puzzle that must contain 1-9
+    if ri < 3:
+        bir = 0
+    elif ri >= 3 and ri <= 5:
+        bir = 1
+    elif ri >= 6:
+        bir = 2
+
+    if ci < 3:
+        bic = 0
+    elif ci >= 3 and ci <= 5:
+        bic = 1
+    elif ci >= 6:
+        bic = 2
+
+    boxi = (bir, bic)
+
+    # check against other numbers in the box
+    for row in puzzle[boxi[0]*3: (boxi[0]*3) + 3]:
+        for n in row[boxi[1]*3: (boxi[1] *3)  + 3]:
+            if n in poss:
+                poss.pop(poss.index(n))
+
     return poss
-                     
+
 
 def find_empty(puzzle):
     for row in puzzle:
@@ -38,20 +69,19 @@ def solve(puzzle):
         # get row and column indices of empty cell
         ri, ci = find_empty(puzzle)[0], find_empty(puzzle)[1]
 
-        # try all possibilites
-        for n in get_poss(puzzle, ri, ci):
-            puzzle[ri][ci] = n
+    # try all possibilites
+    for n in get_poss(puzzle, ri, ci):
+        puzzle[ri][ci] = n
 
-            # recursive call with the updated puzzle
-            if solve(puzzle):
-                return True
-            # reset (line didn't work)
-            else:
-                puzzle[ri][ci] = 0
+        # recursive call with the updated puzzle
+        if solve(puzzle):
+            return True
+        # reset (line didn't work)
+        else:
+            puzzle[ri][ci] = 0
 
-        return False
-                        
-                    
+    return False
+
 if __name__== "__main__":
 
     zero = [[0,0,0,0,0,0,0,0,0],
@@ -64,16 +94,16 @@ if __name__== "__main__":
             [0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0]]
 
-    puzzle = [[0,8,0,0,0,0,0,0,4],
-            [0,2,0,1,9,0,7,0,0],
-            [0,0,9,5,0,8,0,2,0],
-            [3,9,0,8,0,0,4,6,0],
-            [4,0,2,0,6,0,5,0,8],
-            [0,6,8,0,0,2,0,1,7],
-            [0,7,0,9,0,6,2,0,0],
-            [0,0,6,0,8,1,0,7,0],
-            [9,0,0,0,0,0,0,4,0]]
-
+    puzzle =[[3,0,6,5,0,8,4,0,0], 
+          [5,2,0,0,0,0,0,0,0], 
+          [0,8,7,0,0,0,0,3,1], 
+          [0,0,3,0,1,0,0,8,0], 
+          [9,0,0,8,6,3,0,0,5], 
+          [0,5,0,0,9,0,6,0,0], 
+          [1,3,0,0,0,0,2,5,0], 
+          [0,0,0,0,0,0,0,7,4], 
+          [0,0,5,2,0,6,3,0,0]] 
+    
     if solve(puzzle):
         print_puzzle(puzzle)
     else:
